6f0d7ba8b8427f9bd7fe1483e0eb9c589defd5c0
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-08-03 22:18:52 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Review from './components/Review';
 function App() {
   return (
     <div className="App flex flex-col justify-center items-center ">
-      <h1 className='pb-[50px] text-[40px] text-sky-600'>Reviews</h1>
+      <h1 className='pb-[50px] text-[40px] text-blue-800'>Reviews</h1>
       <div className='w-[40%] h-[fit-content]'>
       <Review/>
       </div>
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import people from "./data"
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 const Review = () => {
     const[index,setIndex] = useState(0)
     const{id,name,job,image,text} = people[index]
@@ -46,14 +47,16 @@ const Review = () => {
     <div className=''>
         <div className='flex flex-col justify-center items-center shadow-xl rounded-xl bg-slate-100 p-10'>
             <img src={image} alt={name} className="w-[200px] h-[200px] rounded-full"/>
-            <h2>{name}</h2>
-            <h3>{job}</h3>
-            <h3>{text}</h3>
+            <h2 className='text-[20px] font-bold pt-5'>{name}</h2>
+            <h2 className='text-blue-400 uppercase'>{job}</h2>
+            <br/>
+            <h3 className=''>{text}</h3>
+            <br/>
             <div>
-                <button onClick={prevPerson}>Back</button>
-                <button onClick={nextPerson}>Next</button>
+                <button onClick={prevPerson}><FaChevronLeft/></button>
+                <button onClick={nextPerson}><FaChevronRight/></button>
             </div>
-            <button onClick={randomPerson}>Random</button>
+            <button className='border-2 border-blue-900 bg-sky-200 px-1 ' onClick={randomPerson}>Random</button>
         </div>
     </div>
   )
